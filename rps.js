f777e8482ef00085229bf5904bfdbac705c2bbff
Magnimius <ziyaadshaboodien@gmail.com>
2023-12-12 22:38:11 +0200

    Added basic java UI functionality

--- rps.js
+++ rps.js
@@ -5,25 +5,63 @@ function getComputerChoice() {
 
 function numToRSP(num) {
     if (num == 0) {
-        console.log('rock');
+        concsole.log('rock');
+        compPic.src="./images/blackrock.png";
+        compPic.style.height="130px";
+        compPic.style.width="120px";
         return 'rock';
     }
     if (num == 1) {
         console.log('paper');
+        compPic.src="./images/blackpaper.png";
+        compPic.style.height="130px";
+        compPic.style.width="120px";
         return 'paper';
     }
     if (num == 2) {
         console.log('scissors');
+        compPic.src="./images/blackscissors.png";
+        compPic.style.height="130px";
+        compPic.style.width="120px";
         return 'scissors';
     }
 }
-function getPlayerChoice() {
-    let playernum = parseInt(prompt("Enter either 0, 1 or 2 for Rock, Paper, Scissors"));
-    return numToRSP(playernum);
-}
+// function getPlayerChoice() {
+//     let playernum = parseInt(prompt("Enter either 0, 1 or 2 for Rock, Paper, Scissors"));
+//     return numToRSP(playernum);
+// }
 
+// player selection
+const playerPic = document.getElementById('playerPic');
+const playRock = document.getElementById('rock');
+const playPaper = document.querySelector('#paper');
+const playScissors = document.querySelector('#scissors');
+
+playRock.addEventListener('click',function(e) {
+  playerPic.src="./images/blackrock.png";
+  playerPic.style.height="130px"
+  playerPic.style.widows="120px"
+  playerSelection = 'rock';
+});
+
+playPaper.addEventListener('click',function(e) {
+  playerPic.src="./images/blackpaper.png";
+  playerPic.style.height="130px";
+  playerPic.style.widows="120px";
+  playerSelection = 'paper';
+});
+
+playScissors.addEventListener('click',function(e) {
+  playerPic.src="./images/blackscissors.png";
+  playerPic.style.height="130px"
+  playerPic.style.widows="120px"
+  playerSelection = 'scissors';
+});
+
+/* Computer Selection */
+const compPic = document.getElementById('compPic');
 let computerSelection = getComputerChoice();;
-let playerSelection = getPlayerChoice();
+
 let message;
 function playRound(player, comp) {
     if (
