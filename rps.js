229addb107b3fd4f2959133f4abd6b0690cf7ef2
Magnimius <ziyaadshaboodien@gmail.com>
2023-12-12 13:12:13 +0200

    Finished basic java

--- rps.js
+++ rps.js
@@ -1,23 +1,42 @@
 function getComputerChoice() {
     num = Math.floor(Math.random()*3);
+    return numToRSP(num);
+}
+
+function numToRSP(num) {
     if (num == 0) {
-        console.log("rock");
+        console.log('rock');
+        return 'rock';
     }
     if (num == 1) {
-        console.log("paper");
+        console.log('paper');
+        return 'paper';
     }
     if (num == 2) {
-        console.log("scissors");
+        console.log('scissors');
+        return 'scissors';
     }
 }
-let computerSelection = getComputerChoice();
-let playerSelection = "rock";
-console.log(playerSelection);
-
-function playRound(player, computer) {
-    if ((player == "rock") && (computer == "scissors")) {
-        return "You won!"
-    }
+function getPlayerChoice() {
+    let playernum = parseInt(prompt("Enter either 0, 1 or 2 for Rock, Paper, Scissors"));
+    return numToRSP(playernum);
 }
 
-console.log(playRound(playerSelection, computerSelection));
+let computerSelection = getComputerChoice();;
+let playerSelection = getPlayerChoice();
+let message;
+function playRound(player, comp) {
+    if (
+        (player == 'rock' && comp == 'scissors') ||
+        (player == 'scissors' && comp == 'paper') ||
+        (player == 'paper' && comp == 'rock')
+      ) {
+        message = "You win!!!"
+      } else if (player == comp) {
+        message = "It\'s a tie :/"
+      } else {
+        message = "You lose :("
+      }
+}
+playRound(playerSelection,computerSelection)
+console.log(message);
